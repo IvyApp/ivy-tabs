d30782b6ed1e125655542dc5eb3c88356fde5776
Dray Lacy <dray@envylabs.com>
2014-11-25 23:04:14 -0500

    Add an action handler to the dummy app.
    This fixes deprecation warnings about attaching action handlers directly
    to a view.

--- tests/dummy/app/components/ivy-tab.js
+++ tests/dummy/app/components/ivy-tab.js
@@ -4,6 +4,12 @@ import IvyTabComponent from 'ivy-tabs/components/ivy-tab';
 export default IvyTabComponent.extend({
   tagName: 'li',
 
+  actions: {
+    select: function() {
+      this.select();
+    }
+  },
+
   panelHref: Ember.computed(function() {
     return '#' + this.get('aria-controls');
   }).property('aria-controls')
