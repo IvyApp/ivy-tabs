4e28513d823b29f4fbaea5a9ec07c6062aa10ce5
Dray Lacy <dray@envylabs.com>
2015-03-12 11:32:10 -0400

    Include the entire bootstrap tree in dummy app output.

--- Brocfile.js
+++ Brocfile.js
@@ -2,6 +2,8 @@
 /* global require, module */
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var pickFiles = require('broccoli-static-compiler');
+var path = require('path');
 
 var app = new EmberAddon();
 
@@ -17,7 +19,11 @@ var app = new EmberAddon();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/ember/ember-template-compiler.js');
+app.import(path.join(app.bowerDirectory, 'ember/ember-template-compiler.js'), { type: 'test' });
 
-module.exports = app.toTree();
+var bootstrapTree = pickFiles(path.join(app.bowerDirectory, 'bootstrap/dist/css'), {
+  srcDir: '/',
+  destDir: '/assets'
+});
+
+module.exports = app.toTree([bootstrapTree]);
