8b3fd7a650ee20c2b5558bf3b077d9faff0b6952
Dray Lacy <dray@envylabs.com>
2015-11-19 17:48:27 -0500

    Upgrade to ember-cli 1.13.13.

--- config/environment.js
+++ config/environment.js
@@ -1,21 +1,6 @@
+/*jshint node:true*/
 'use strict';
 
-var name = require('../package.json').name;
-
-module.exports = function(environment) {
-  environment = environment || 'development';
-
-  var defaultEnv = {
-    environment: environment,
-    addonPrefix: name,
-    shimName: name + '-shim'
-  };
-
-  var ENV = {
-    development: defaultEnv,
-    test: defaultEnv,
-    production: defaultEnv
-  };
-
-  return ENV[environment];
+module.exports = function(/* environment, appConfig */) {
+  return { };
 };
