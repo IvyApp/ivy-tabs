568877c587a9243eecc90d4cd07d55928af54c73
Dray Lacy <dray@envylabs.com>
2016-08-31 15:24:06 -0400

    Update dummy app config to use `rootURL`

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.baseURL = '/ivy-tabs/';
+    ENV.rootURL = '/ivy-tabs/';
 
   }
 
