cc6fd5cc75ab069539319c4f5001f2fb902bc1f5
Nathaniel Bibler <nate@envylabs.com>
2018-03-07 18:56:47 -0500

    Remove Bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,8 +13,8 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css.map');
 
   return app.toTree();
 };
