cac6241888fdc592639959c0f4f0ceaf1e28e43f
Dray Lacy <dray@envylabs.com>
2019-04-02 23:45:38 -0400

    chore: add commitizen and commitlint
    These enforce the conventional-changelog standard. In place of `git commit`, use `yarn git-cz` to
    help you write commit messages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
       files: [
         '.eslintrc.js',
         '.template-lintrc.js',
+        'commitlint.config.js',
         'ember-cli-build.js',
         'index.js',
         'testem.js',
